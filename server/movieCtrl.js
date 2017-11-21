62d1c68ff52fbc36f80e00530b968aaa2d0bd0e5
meganmfisher <meganmichellefisher@gmail.com>
2017-11-20 20:27:03 -0700

    notes

--- server/movieCtrl.js
+++ server/movieCtrl.js
@@ -24,3 +24,5 @@ module.exports = {
 
 
 // Using the /discover/movie endpoint and searching by year, I requested for all years from 1930-2016 and the corresponding pages. This way I got 230,294 movies. I am not sure if there is a limit to the results of /discover/movie, or maybe some movies don't have year? Currently the latest id for a movie is 402985...I ran my script in 4 child processes (only 4 in order to not exceed the request limit) and requesting and storing them in db took ~8h. The downside is that /discover/movie/ does not give you the full movie info. So after I got all the movie ids I requested /movie/:id for all of the movies. That took ~12h.
+
+//loop through pages in recursive function push each time to array before sending the array to the front end. 
